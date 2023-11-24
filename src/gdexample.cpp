135237a727a1f06deca8c40ed15f0f181c04bdfc
BluBloos <cnoah1705@gmail.com>
2023-11-24 18:19:36 -0500

    project init.
    we have a GDExtension that prints hello world to the Godot Editor every
    1 second.

---
+++ src/gdexample.cpp
@@ -0,0 +1,72 @@
+#include "gdexample.h"
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void Beta::_bind_methods() {
+}
+
+Beta::Beta() {
+	// Initialize any variables here.
+	time_passed = 0.0;
+}
+
+Beta::~Beta() {
+	// Add your cleanup here.
+}
+
+void Beta::_process(double delta) {
+	time_passed += delta;
+
+	Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 2.0)), 10.0 + (10.0 * cos(time_passed * 2.0)));
+
+	set_position(new_position);
+
+    // have I hit the thing ?
+
+// get by tag
+
+
+
+
+}
+
+void The_Game::_bind_methods()
+{}
+
+void The_Game::_get_property_list(List<PropertyInfo> *p_list) const
+{
+
+	p_list->push_back(
+        PropertyInfo(Variant::INT, "My Property", PROPERTY_HINT_RANGE, "0,30,1", PROPERTY_USAGE_EDITOR));
+    
+}
+
+double runningTime = 0.f;
+
+void The_Game::_ready()
+{
+    node = get_node<Node>("printer_node");
+}
+
+void The_Game::_process(double delta)
+{
+    runningTime += delta;
+
+    if (runningTime > 1.f)
+    {
+        // do something interesting.
+        const char *str = "we rich";        
+        double something = 5.1;
+        static int d = 3;
+//        printf( "%s, %f, %d", str, something, d++ );
+
+// can we get into a debugger?
+        node->call( "my_print" ); 
+
+
+      runningTime = 0.f;
+    }
+    
+}
+
---
+++ src/gdexample.h
@@ -0,0 +1,43 @@
+#ifndef GDEXAMPLE_H
+#define GDEXAMPLE_H
+
+#include <godot_cpp/classes/sprite2d.hpp>
+
+namespace godot {
+
+class The_Game : public Node {
+        GDCLASS(The_Game, Node)
+
+       void _get_property_list(List<PropertyInfo> *p_list) const;
+
+    static void _bind_methods();
+// TODO: it seems like the above thing here is a GDExtension
+// specific thing.
+
+    Node *node;
+
+public:
+    void _ready();
+    void _process(double delta);
+
+    };
+
+class Beta : public Sprite2D {
+	GDCLASS(Beta, Sprite2D)
+
+private:
+	double time_passed;
+
+protected:
+	static void _bind_methods();
+
+public:
+	Beta();
+	~Beta();
+
+	void _process(double delta);
+};
+
+}
+
+#endif
